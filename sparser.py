27abed9d93961ba2ed8409d44b8b9f92605e25e7
Alice Ferrazzi <alicef@gentoo.org>
2021-02-12 04:25:59 +0900

    Add auto generated passwords and irc when testing
    Add script for converting docker-compose strings to secrets
    
    Signed-off-by: Alice Ferrazzi <alicef@gentoo.org>

--- sparser.py
+++ sparser.py
@@ -1,12 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import yaml, re
+import yaml
+import re
+import os
+import secrets
+import string
+from random import randint
 
-def secrets():
-    with open("secrets.yml", "r") as ymlfile:
-        cfg = yaml.load(ymlfile)
-    return cfg
+
+def generate_password(password_length):
+    alphabet = string.ascii_letters +string.digits
+    characters = [secrets.choice(alphabet) for i in range(password_length)]
+    password = ''.join(characters)
+    return password
+
+def secrets_file():
+    if os.path.isfile('./secrets.yml'):
+        with open("secrets.yml", "r") as ymlfile:
+            cfg = yaml.load(ymlfile)
+        return cfg
+    else:
+        with open("secrets.yml.example", "r") as ymlfile:
+            cfg = yaml.load(ymlfile)
+        return cfg
 
 def input_file():
     with open("docker-compose.yml", "r") as inputfile:
@@ -17,11 +34,17 @@ def output_file(output):
     with open("docker-compose.yml", "w+") as outputfile:
         outputfile.write(output)
 
-secret_list = secrets()
-document = input_file()
-for category in secret_list['secrets']:
-    for string in secret_list['secrets'][category]:
-        print(string)
-        document=document.replace(string, str(secret_list['secrets'][category][string]))
-
-output_file(document)
+if __name__ == '__main__':
+    secret_list = secrets_file()
+    document = input_file()
+    for category in secret_list['secrets']:
+        for namekey in secret_list['secrets'][category]:
+            print(namekey)
+            if str(secret_list['secrets'][category][namekey]) == "GENERATE_PASS":
+                password = generate_password(8)
+                document=document.replace(namekey, generate_password(8))
+            elif str(secret_list['secrets'][category][namekey]) == "GENERATE_NICK":
+                document=document.replace(namekey, "gkci_"+ str(randint(1, 100)))
+            else:
+                document=document.replace(namekey, str(secret_list['secrets'][category][namekey]))
+    output_file(document)
--- start_testing.sh
+++ start_testing.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp kernelci-ci-gkernelci.json.example GBuildbot-worker/.kernelci-ci-gkernelci.json
+python sparser.py
 docker-compose up -d
