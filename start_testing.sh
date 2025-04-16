fee8e7b376e432661dcdd73d9912f4706fbfb90f
Jesse Simpson <jesse.simpson@camunda.com>
2025-04-16 18:51:35 -0400

    test: replace travisci with github actions
    Signed-off-by: Jesse Simpson <jesse.simpson@camunda.com>

--- start_testing.sh
+++ start_testing.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp kernelci-ci-gkernelci.json.example GBuildbot-worker/.kernelci-ci-gkernelci.json
+sed -i "s/your local docker GID/$(cat /etc/group | grep docker | cut -d: -f3)/" docker-compose.yml.template
 python sparser.py || exit $?
-docker-compose up -d
+docker compose up -d
